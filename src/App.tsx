4f4f913e5420e119a380eb32947a0d3b3e7108f9
Mateo Miccino <mateomiccino@gmail.com>
2021-08-04 11:52:25 -0300

    feat: download & play features for the launcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+const { ipcRenderer } = window.require('electron');
 
-function App() {
+enum DownloadState {
+  None = 0,
+  Downloading,
+  ReadyToPlay
+}
+
+const App = () => {
+  const [progress, setProgress] = useState<number>(0)
+  const [downloadState, setDownloadState] = useState<DownloadState>(DownloadState.None)
+  useEffect(() => {
+    ipcRenderer.on("downloadStart", (event: any) => {
+      setProgress(0)
+      setDownloadState(DownloadState.Downloading)
+    })
+    ipcRenderer.on("downloadProgress", (event: any, percent: number) => {
+      setProgress(Math.round(percent))
+    })
+    ipcRenderer.on("downloadCompleted", (event: any) => {
+      setProgress(100)
+      setDownloadState(DownloadState.ReadyToPlay)
+    })
+  }, [])
+
+  const downloadArtifacts = () => {
+    console.log("downloadArtifacts")
+    ipcRenderer.send('download-button', { url: "https://renderer-artifacts.decentraland.org/desktop/main/unity-renderer-linux.zip" })
+  }
+
+  const playGame = () => {
+    ipcRenderer.send('execute-process')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Progress: {progress}%
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={downloadArtifacts} disabled={downloadState != DownloadState.None}>
+          Download
+        </button>
+        <button onClick={playGame} disabled={downloadState != DownloadState.ReadyToPlay}>
+          Play
+        </button>
       </header>
     </div>
   );
