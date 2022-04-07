9086089bcc52335d4f17996f85e7f8d78cdaecc0
Leandro Mendoza <leandro@decentraland.org>
2022-04-07 19:38:01 -0300

    feat: remove checkampersand (#47)
    * fix window
    
    remove checkampersand
    
    * remove building url as string concatenation and add using the URL class
    
    * increment version

--- electron/window.ts
+++ electron/window.ts
@@ -3,7 +3,7 @@ import { registerUpdaterEvents } from './updater'
 import * as path from 'path'
 import { main } from './main'
 import * as isDev from 'electron-is-dev'
-import { checkAmpersand, getAppTitle, getIconByPlatform, getKeyAndValue, onExit } from './helpers'
+import { getAppTitle, getIconByPlatform, onExit } from './helpers'
 import { URLSearchParams } from 'url'
 
 export const createWindow = async (title: string, launcherPaths: LauncherPaths): Promise<BrowserWindow> => {
@@ -111,29 +111,51 @@ export const loadDecentralandWeb = async (win: BrowserWindow) => {
     showLoading(win)
 
     const stage = main.config.developerMode ? 'zone' : 'org'
-    let url = `http://play.decentraland.${stage}/?`
+    const url = new URL(
+      main.config.customUrl || `http://play.decentraland.${stage}/?renderer-version=loading`
+    )
 
-    if (main.config.customUrl) {
-      url = main.config.customUrl
-    } else {
-      url = `${url}renderer-version=loading&`
+    const customParamObj = new URLSearchParams(main.config.customParams)
+    for (const [key, value] of Array.from(customParamObj.entries())) {
+      url.searchParams.append(key, value)
     }
 
-    url = `${url}${main.config.customParams}${main.config.defaultParams}ws=wss://localhost:${main.config.port}/dcl`
+    const defaultParamObj = new URLSearchParams(main.config.defaultParams)
+    for (const [key, value] of Array.from(defaultParamObj.entries())) {
+      url.searchParams.append(key, value)
+    }
 
-    console.log(`Opening: ${url}`)
+    url.searchParams.append('ws', `wss://localhost:${main.config.port}/dcl`)
 
-    let promise = win.loadURL(url)
+    console.log(`Opening: ${url.toString()}`)
+
+    let promise = win.loadURL(url.toString())
     promise.finally(() => hideLoading(win))
   } catch (err) {
     console.error('err:', err)
   }
 }
 
+/**
+ * Crop 'dcl://' at begin and a slash at the end if exists.
+ * @param url - to be process
+ * @returns URL proccesed
+ */
+function cropUrl(url: string): string {
+  let result: string = url
+  if (url.startsWith('dcl://')) {
+    result = result.substring('dcl://'.length)
+  }
+  if (url.endsWith('/')) {
+    result = result.substring(0, result.length - 1)
+  }
+  return result
+}
+
 export const onOpenUrl = (data: string, win?: BrowserWindow) => {
   main.config = { ...main.defaultConfig }
 
-  const params = new URLSearchParams(data.substring('dcl://'.length))
+  const params = new URLSearchParams(cropUrl(data))
 
   const desktopBranch: string | null = params.get('DESKTOP-BRANCH')
   if (desktopBranch != null) {
@@ -149,7 +171,7 @@ export const onOpenUrl = (data: string, win?: BrowserWindow) => {
   const previewMode: string | null = params.get('PREVIEW-MODE')
   if (previewMode != null) {
     if (previewMode !== '') {
-      main.config.customUrl = checkAmpersand(previewMode)
+      main.config.customUrl = previewMode
       main.config.previewMode = true
     }
     params.delete('PREVIEW-MODE')
