07726a40bb5dcc0080717c6c80e3d3d48c47d314
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2022-04-21 09:41:04 -0300

    fix: stricting the certificate-error, only accepting 'wss://localhost:7666/dcl' url with 'net::ERR_CERT_AUTHORITY_INVALID' error.

--- electron/main.ts
+++ electron/main.ts
@@ -178,8 +178,10 @@ const startApp = async (): Promise<void> => {
 app.on('certificate-error', (event, webContents, url, error, certificate, callback) => {
   // On certificate error we disable default behaviour (stop loading the page)
   // and we then say "it is all fine - true" to the callback
-  event.preventDefault()
-  callback(true)
+  if (url === 'wss://localhost:7666/dcl' && error === 'net::ERR_CERT_AUTHORITY_INVALID') {
+    event.preventDefault()
+    callback(true)
+  }
 })
 
 app.whenReady().then(async () => {
