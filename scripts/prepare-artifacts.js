0368a4c56c27a247f1981d187da76b7f0a218e36
frami <i@2fd.me>
2023-03-13 13:20:39 -0300

    chore: use local path

--- scripts/prepare-artifacts.js
+++ scripts/prepare-artifacts.js
@@ -1,20 +1,27 @@
-const fs = require('fs');
-const dir = './output';
-const ext = process.argv[2];
-console.log('ext: ', ext);
+const fs = require('fs')
+const dir = './output'
+const ext = process.argv[2]
+console.log('ext: ', ext)
 if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
+  console.log('creating', dir)
+  fs.mkdirSync(dir)
+}
+
+function copyFileSync(src, dest, mode) {
+  console.log('coping', src, 'to', dest)
+  fs.copyFileSync(src, dest, mode)
 }
 
 if (ext === 'exe') {
-    if (fs.existsSync(`dist/Install Decentraland.${ext}`)) {
-        fs.copyFileSync(`dist/Install Decentraland.${ext}`, `output/Install Decentraland.${ext}`)
-    }
+  if (fs.existsSync(`dist/Install Decentraland.${ext}`)) {
+    copyFileSync(`dist/Install Decentraland.${ext}`, `output/Install Decentraland.${ext}`)
+  }
 
-    if (fs.existsSync(`dist/Decentraland.appx`)) {
-        fs.copyFileSync(`dist/Decentraland.appx`, `output/Decentraland.appx`)
-    }
+  if (fs.existsSync(`dist/Decentraland.appx`)) {
+    copyFileSync(`dist/Decentraland.appx`, `output/Decentraland.appx`)
+  }
 } else {
-    fs.copyFileSync(`dist/Decentraland.${ext}`, `output/Decentraland.${ext}`)
+  copyFileSync(`dist/Decentraland.${ext}`, `output/Decentraland.${ext}`)
 }
-console.log('Done');
+
+console.log('Done')
